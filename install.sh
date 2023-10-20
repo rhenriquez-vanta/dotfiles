ab2b27bf940504becfb7e3076886a6a654aa0ae6
Roy Henriquez <roy.henriquez@gmail.com>
2023-10-20 15:23:13 -0600

    init (#1)
    ---
    
    <details open="true"><summary>Generated summary (powered by <a href="https://app.graphite.dev">Graphite</a>)</summary>
    
    > ## TL;DR
    > This pull request introduces changes to the `.zshrc` file and adds a new `install.sh` script. The changes include enabling Powerlevel10k instant prompt, setting up the Powerlevel10k theme, and installing fonts. 
    > 
    > ## What changed
    > - A new `.zshrc` file has been added with configurations for Powerlevel10k instant prompt, setting the ZSH theme to Powerlevel10k, and enabling plugins like git, vscode, and zsh-autosuggestions.
    > - A new `install.sh` script has been added which creates symbolic links for all dotfiles in the home directory, installs Powerline fonts, and sets up the Powerlevel10k theme.
    > 
    > ## How to test
    > 1. Pull the changes from this branch into your local environment.
    > 2. Run the `install.sh` script using the command `./install.sh`.
    > 3. Open a new terminal window to see the changes take effect.
    > 4. You can also manually inspect the `.zshrc` file to see the new configurations.
    > 
    > ## Why make this change
    > These changes are made to improve the terminal user experience by enabling a more informative and visually appealing prompt using the Powerlevel10k theme. The `install.sh` script automates the process of setting up the environment, making it easier to maintain and replicate the setup across different machines.
    </details>

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+
+create_symlinks
+
+echo "Installing fonts."
+FONT_DIR="$HOME/.fonts"
+git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
+cd $FONT_DIR
+./install.sh
+
+echo "Setting up the Powerlevel10k theme."
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
